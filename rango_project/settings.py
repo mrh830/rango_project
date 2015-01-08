e6bb54ed531759e24c6961990c559b0123d750b5
EternalIsland <xenophile999@gmail.com>
2015-01-08 12:26:50 -0330

    Added basic template.

--- rango_project/settings.py
+++ rango_project/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -83,5 +84,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'templates'),
 )
