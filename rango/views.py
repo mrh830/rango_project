c0badd464b66f069a1aaf3cc072147b7de8025df
Matthew Hynes <matthew.hynes@mun.ca>
2015-01-22 19:18:01 -0330

    Added category form

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from rango.models import Category, Page
 from django.http import HttpResponse
+
+from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 
 def index(request):
@@ -9,6 +11,9 @@ def index(request):
 
     category_list = Category.objects.all()
     context_dict = {'categories': category_list}
+
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
 
     return render_to_response('rango/index.html', context_dict, context)
 
@@ -37,3 +42,22 @@ def category(request, category_name_url):
         pass
 
     return render_to_response('rango/category.html', context_dict, context)
+
+
+def add_category(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+
+            return index(request)
+        else:
+            print form.errors
+
+    else:
+        form = CategoryForm()
+
+    return render_to_response('rango/add_category.html', {'form': form}, context)
